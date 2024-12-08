e025cdc3eaf83f4505d6c21962c5088200cd97c4
Phoenix <55042406+ThePhoenix78@users.noreply.github.com>
2024-12-08 17:17:54 +0100

    Add files via upload

--- ui.py
+++ ui.py
@@ -44,48 +44,48 @@ root = tk.Tk()
 root.title("Lumikki tool")
 
 label_symbol = tk.Label(root, text="Symbol order (•↑→↓←)")
-label_symbol.pack()
+label_symbol.pack(pady=[15, 5])
 
 entry_symbol = tk.Entry(root, justify="center")
 entry_symbol.insert(0, "01234")
 entry_symbol.pack(padx=50)
 
 label_chaine1 = tk.Label(root, text="First group")
-label_chaine1.pack()
+label_chaine1.pack(pady=[7, 5])
 
 entry_chaine1 = tk.Entry(root, justify="center")
 entry_chaine1.insert(0, "lrb")
 entry_chaine1.pack(padx=50)
 
 label_chaine2 = tk.Label(root, text="Second group")
-label_chaine2.pack()
+label_chaine2.pack(pady=[7, 5])
 
 entry_chaine2 = tk.Entry(root, justify="center")
 entry_chaine2.insert(0, "rlt")
-entry_chaine2.pack()
+entry_chaine2.pack(padx=50)
 
 label_entier = tk.Label(root, text="Base")
-label_entier.pack()
+label_entier.pack(pady=[7, 5])
 
 entry_entier = tk.Entry(root, justify="center")
 entry_entier.insert(0, "5")
-entry_entier.pack()
+entry_entier.pack(padx=50)
 
 label_offset = tk.Label(root, text="Offset (leave empyt to not convert into text)")
-label_offset.pack()
+label_offset.pack(pady=[7, 5])
 
 entry_offset = tk.Entry(root, justify="center")
 entry_offset.insert(0, "32")
-entry_offset.pack()
+entry_offset.pack(padx=50, pady=[0, 7])
 
-button_concatener = tk.Button(root, text="Make", command=update_window)
-button_concatener.pack(pady=10)
+button_concatener = tk.Button(root, text="Generate", command=update_window)
+button_concatener.pack(pady=[10, 10])
 
 text_resultat = tk.Text(root, width=145, height=12, wrap=tk.WORD)
-text_resultat.pack(fill='x', padx=50, pady=20)
+text_resultat.pack(fill='x', padx=50, pady=10)
 
 button_copy = tk.Button(root, text="Copy", command=copy_to)
-button_copy.pack(pady=10)
+button_copy.pack(pady=[7, 20])
 
 # root.geometry("950x600")
 root.mainloop()
